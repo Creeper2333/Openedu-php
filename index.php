09b623fbb6be3a4820ac46316c784b82c2a8d352
Creeper233 <52315359+Creeper2333@users.noreply.github.com>
2020-11-22 14:23:15 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -7,9 +7,26 @@
 .STYLE5 {color: #FFFFFF}
 .STYLE6 {color: #FFFFFF; font-family: "黑体"; }
 .STYLE8 {font-family: "黑体"; font-size: x-small; }
+a:link {
+	text-decoration: none;
+}
+a:visited {
+	text-decoration: none;
+}
+a:hover {
+	text-decoration: none;
+}
+a:active {
+	text-decoration: none;
+}
 -->
 </style>
 <body>
+ <script>
+function w(){
+alert("功能未开放")
+}
+</script>
 <style type="text/css">
 <!--
 .STYLE1 {
@@ -24,21 +41,35 @@
 -->
 </style>
 <!--到时候写自适应 -->
-<div class="topfrm" id="a"><table width="1300" height="50" border="0" cellpadding="0" cellspacing="0">
+<div class="topfrm" id="a"><table width="1300" height="100" border="0" cellpadding="0" cellspacing="0">
   <tr>
-    <th width="900" height= $w bordercolor="#FFFFFF" bgcolor="#fb7299" scope="col" ><div align="left"><span class="STYLE1 STYLE5"><span class="STYLE3">OpenEdu Platform</span> <span class="STYLE2 STYLE4">学习软件开源平台</span></span></div></th>
-    <th width="100" bordercolor="#FFFFFF" bgcolor="#fb7299" scope="col"><span class="STYLE6"><a href="download.php" class="STYLE5">下载</a></span></th>
-    <th width="100" bordercolor="#FFFFFF" bgcolor="#fb7299" class="STYLE6" scope="col"><a href="donate.php" class="STYLE6">捐赠</a></th>
-    <th width="100" bordercolor="#FFFFFF" bgcolor="#fb7299" class="STYLE6" scope="col"><a href="joinus.php" class="STYLE6">加入我们</a></th>
-    <th width="100" bordercolor="#FFFFFF" bgcolor="#fb7299" class="STYLE6" scope="col"><a href="apidocs.php" class="STYLE5">API</a></th>
+    <th width="900" height= $w bordercolor="#FFFFFF" bgcolor="#fb7299" scope="col" ><div align="left">
+      <h1><span class="STYLE1 STYLE5"><span class="STYLE3">OpenEdu Platform</span> <span class="STYLE2 STYLE4">学习软件开源平台</span></span></h1>
+    </div></th>
+    <th width="100" bordercolor="#FFFFFF" bgcolor="#fb7299" scope="col"><h3><span class="STYLE6"><a href="download.php" class="STYLE5">下载</a></span></h3></th>
+    <th width="100" bordercolor="#FFFFFF" bgcolor="#fb7299" class="STYLE6" scope="col"><h3><a href="donate.php" class="STYLE6">捐赠</a></h3></th>
+    <th width="100" bordercolor="#FFFFFF" bgcolor="#fb7299" class="STYLE6" scope="col"><h3><a href="joinus.php" class="STYLE6">加入我们</a></h3></th>
+    <th width="100" bordercolor="#FFFFFF" bgcolor="#fb7299" class="STYLE6" scope="col"><h3><a href="apidocs.php" class="STYLE5 STYLE3">API</a></h3></th>
   </tr>
 </table></div>
 
 <div class="mfrm" id="b">
 <label>
-<span class="STYLE4">「破解的目的不是制造麻烦，<br>
-  是为了让垃圾不再垃圾!」
-</span></label>
+<div align="center"><span class="STYLE4">「破解的目的不是制造麻烦，是为了让垃圾不再垃圾!」
+</span></div>
+</label>
+</div>
+<div>
+<h2 align="center">立即获取我们的<strong>免费工具！</strong></h2>
+<div align="center">
+  <table width="1106" height="75" border="1" cellpadding="0" cellspacing="0">
+    <tr>
+      <th width="550" valign="middle" bgcolor="#fb7299" scope="col"><div align="left">
+        <h3 align="center" class="STYLE1"><a href="EkwingCracker.exe" class="STYLE6">下载 破解版翼课网 for Windows (64-bit)</a> </h3>
+      </div></th>
+      <th width="550" bgcolor="#fb7299" scope="col" onclick="w()"><h3 align="center" class="STYLE1"><a class="STYLE5"><span class="STYLE6">下载 破解版翼课网 for Windows (32-bit) </span></span></h3></th>
+      </tr>
+  </table>
 </div>
 <p>&nbsp;</p>
 <div align="center"><span class="STYLE8">前端 by Creeper2333 </span>
